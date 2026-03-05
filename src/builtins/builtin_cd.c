d9c0b3444b8ebab2d73a625f426ea2f0bf1f1895
Barbara Moreira Drummond De Almeida <bmoreira@c1r7p2.42sp.org.br>
2026-03-05 19:26:04 -0300

    make builtin cd more solid

--- src/builtins/builtin_cd.c
+++ src/builtins/builtin_cd.c
@@ -6,35 +6,35 @@
 /*   By: bmoreira <bmoreira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 01:34:24 by bmoreira          #+#    #+#             */
-/*   Updated: 2026/02/27 22:14:32 by bmoreira         ###   ########.fr       */
+/*   Updated: 2026/03/05 19:23:08 by bmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	builtin_cd(t_list **vars, char **args)
+int	builtin_cd(t_list **vars, char **args)
 {
-	char	new_pwd[1024];
-	char	*old_pwd;
-	char	*path;
+	char	buffer[1024];
+	char	*dir;
+	int		i;
 
-	path = *(args + 1);
-	if (!path || ft_strcmp(path, "~") == 0)
-		path = var_get_value(*vars, "HOME");
-	if (!path && printf("cd: HOME not set\n"))
-		return ;
-	if (chdir(path) == -1 && printf("cd: %s: no such file or directory\n", path))
-		return ;
-	old_pwd = var_get_value(*vars, "PWD");
-	var_set(vars, ft_strdup("OLD_PWD"), ft_strdup(old_pwd), TRUE);
-	if (getcwd(new_pwd, sizeof(new_pwd)) == -1)
-		return ;
-	var_set(vars, ft_strdup("PWD"), ft_strdup(new_pwd), TRUE);
+	i = 1;
+	dir = args[i];
+	if (ft_split_size(args) > 2 && printf("cd: too many arguments\n"))
+		return EXIT_FAILURE;
+	if (!args[i] || ft_strcmp(args[i], "~") == 0)
+		dir = var_get_value(vars, "HOME");
+	if (!dir && printf("cd: HOME not set\n"))
+		return EXIT_FAILURE;
+	if (ft_strcmp(args[i], "-") == 0)
+		dir = var_get_value(vars, "OLDPWD");
+	if (!dir && printf("cd: OLDPWD not set\n"))
+		return EXIT_FAILURE;
+	if (getcwd(buffer, sizeof(buffer)) != NULL)
+		var_set(vars, ft_strdup("OLDPWD"), ft_strdup(buffer), TRUE);
+	if (chdir(dir) == -1 && printf("cd: %s: No such file or directory\n", dir))
+		return EXIT_FAILURE;
+	if (getcwd(buffer, sizeof(buffer)) != NULL)
+		var_set(vars, ft_strdup("PWD"), ft_strdup(buffer), TRUE);
+	return EXIT_SUCCESS;
 }
-
-// cd (empty args) / cd ~ => changes to home
-// cd . => stays on current dir
-// cd .. => changes to previous dir
-// cd / (one or more slashes) => changes to root
-// cd /(one or more slashes)dirname => changes to folder in root
-// cd path => check if exists. if yes, change to path. if not, shows error msg
