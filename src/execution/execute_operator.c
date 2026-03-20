3cb4c99a1bb61bb4235b5f0d01db5e9f50c013e3
bmoreira <bmoreira@student.42sp.org.br>
2026-03-20 20:24:51 -0300

    implemented pipe

--- src/execution/execute_operator.c
+++ src/execution/execute_operator.c
@@ -1,55 +1,82 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   execute.c                                          :+:      :+:    :+:   */
+/*   execute_operator.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bmoreira <bmoreira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/22 03:51:40 by bcosta-b          #+#    #+#             */
-/*   Updated: 2026/03/14 22:19:32 by bmoreira         ###   ########.fr       */
+/*   Updated: 2026/03/20 20:24:23 by bmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
 
-static int	execute_and(t_shell *ctx, t_ast *left, t_ast *right, int is_child)
+static int	pipe_children(t_shell *ctx, t_ast *ast, int pipefd[2], int pid[2])
 {
-	ctx->exit_status = execute(ctx, left, is_child);
+	pid[0] = fork();
+	if (pid[0] < 0)
+		return (EXIT_FAILURE);
+	else if (pid[0] == 0)
+	{
+		dup2(pipefd[1], STDOUT_FILENO);
+		close(pipefd[0]);
+		close(pipefd[1]);
+		execute(ctx, ast->left, TRUE);
+		exit(ctx->exit_status);
+	}
+	pid[1] = fork();
+	if (pid[1] < 0)
+		return (EXIT_FAILURE);
+	else if (pid[1] == 0)
+	{
+		dup2(pipefd[0], STDIN_FILENO);
+		close(pipefd[0]);
+		close(pipefd[1]);
+		execute(ctx, ast->right, TRUE);
+		exit(ctx->exit_status);
+	}
+	return (EXIT_SUCCESS);
+}
+
+static int	execute_pipe(t_shell *ctx, t_ast *ast, int is_child)
+{
+	int	pipefd[2];
+	int	pid[2];
+	int	status;
+
+	pipe(pipefd);
+	pipe_children(ctx, ast, pipefd, pid);
+	close(pipefd[0]);
+	close(pipefd[1]);
+	waitpid(pid[0], NULL, 0);
+	waitpid(pid[1], &status, 0);
+	return (WEXITSTATUS(status));
+}
+
+static int	execute_and(t_shell *ctx, t_ast *ast, int is_child)
+{
+	ctx->exit_status = execute(ctx, ast->left, is_child);
 	if (ctx->exit_status == EXIT_SUCCESS)
-		ctx->exit_status = execute(ctx, right, is_child);
+		ctx->exit_status = execute(ctx, ast->right, is_child);
 	return (ctx->exit_status);
 }
 
-static int	execute_or(t_shell *ctx, t_ast *left, t_ast *right, int is_child)
+static int	execute_or(t_shell *ctx, t_ast *ast, int is_child)
 {
-	ctx->exit_status = execute(ctx, left, is_child);
+	ctx->exit_status = execute(ctx, ast->left, is_child);
 	if (ctx->exit_status != EXIT_SUCCESS)
-		ctx->exit_status = execute(ctx, right, is_child);
+		ctx->exit_status = execute(ctx, ast->right, is_child);
 	return (ctx->exit_status);
 }
-
-// int	execute_pipe(t_shell *shell, t_ast *left, t_ast *right, int is_child)
-// {
-// }
-
-// todo PIPE
-// add in_child to execute argv
-// in_child default is false
-// execute_pipe
-// fork
-// pass in_child as true
-// execute_external_cmd receives in_child
-// execute_external_cmd checks in_child
-// if true -> execve directly (calls execute_in_parent)
-// if false -> fork + execve (calls execute_in_child)
 
 int	execute_operator(t_shell *ctx, t_ast *ast, t_exec_node *node, int is_child)
 {
 	if (node->type == NODE_AND)
-		ctx->exit_status = execute_and(ctx, ast->left, ast->right, is_child);
+		ctx->exit_status = execute_and(ctx, ast, is_child);
 	else if (node->type == NODE_OR)
-		ctx->exit_status = execute_or(ctx, ast->left, ast->right, is_child);
-	// else if (node->type == NODE_PIPE)
-	// 	ctx->exit_status = execute_pipe(ctx, ast->left,	ast->right, is_child);
+		ctx->exit_status = execute_or(ctx, ast, is_child);
+	else if (node->type == NODE_PIPE)
+		ctx->exit_status = execute_pipe(ctx, ast, is_child);
 	return (ctx->exit_status);
 }
