9b2796abb68b697a7228f6d87dbd033441ea03a8
bmoreira <babidrummond@hotmail.com>
2026-03-21 18:34:11 -0300

    implemented split_unquoted and trying to implement expand_string

--- include/expansion.h
+++ include/expansion.h
@@ -3,16 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   expansion.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bmoreira <bmoreira@student.42.fr>          +#+  +:+       +#+        */
+/*   By: barbara.drummond <barbara.drummond@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/20 20:55:14 by bmoreira          #+#    #+#             */
-/*   Updated: 2026/03/20 20:56:42 by bmoreira         ###   ########.fr       */
+/*   Updated: 2026/03/21 17:42:29 by barbara.dru      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXPANSION_H
 # define EXPANSION_H
 
+# include "lexer.h"
 # include "../libft/include/libft.h"
+
+# define QUOTE_GUARD '\x01'
+
+char	**split_unquoted(char *s, char c);
 
 #endif
--- src/expansion/expand.c
+++ src/expansion/expand.c
@@ -3,28 +3,77 @@
 /*                                                        :::      ::::::::   */
 /*   expand.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bmoreira <bmoreira@student.42.fr>          +#+  +:+       +#+        */
+/*   By: barbara.drummond <barbara.drummond@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/20 20:55:06 by bmoreira          #+#    #+#             */
-/*   Updated: 2026/03/21 00:47:00 by bmoreira         ###   ########.fr       */
+/*   Updated: 2026/03/21 18:31:54 by barbara.dru      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "expansion.h"
 
+char	*get_key(char *word)
+{
+	char	*key;
+	int		i;
+
+	i = 0;
+	while (word[i])
+	{
+		if (!ft_isalnum(word[i]) && word[i] != '_')
+			break ;
+		i++;
+	}
+	key = ft_substr(word, 0, i);
+	return (key);
+}
+
+t_word	*expand_string(t_word *words, t_list *vars)
+{
+	t_word	*word;
+	char	*new_str;
+	char	*str;
+	char	*key;
+	int		i;
+
+	word = words;
+	while (word)
+	{
+		str = (char *) word->link.content;
+		new_str = ft_strdup("");
+		i = 0;
+		while (str[i])
+		{
+			if (str[i] == '$' && word->quote_state != QUOTE_SINGLE)
+			{
+				key = get_key(str + i + 1);
+				new_str = ft_strjoin_free(new_str, var_get(vars, key), TRUE, FALSE);
+				i += ft_strlen(key) + 1;
+				free(key);
+			}
+			else
+				new_str = ft_strjoin_free(new_str, str[i++], TRUE, FALSE);
+		}
+		free(word->link.content);
+		word->link.content = new_str;
+		word = word->link.next;
+	}
+	return (EXIT_SUCCESS);
+}
+
 int expand()
 {
-    
+	
 }
 
 // Expansão:
 // 1. Recebe argv em lista
 // 2. Percorre cada item da lista de argv
 // 3. Expandir string (list t_word)
-    // 3.1 Percorrer lista de t_word
-    // 3.2 Verificar se precisa expandir (tem $) e quote_state != single
-    // 3.3 Após $, percorre string até encontrar caractere não válido em key
-    // 3.4 Guarda valor de $ até não válido, procura em env, e expande
+	// 3.1 Percorrer lista de t_word
+	// 3.2 Verificar se precisa expandir (tem $) e quote_state != single
+	// 3.3 Após $, percorre string até encontrar caractere não válido em key
+	// 3.4 Guarda valor de $ até não válido, procura em env, e expande
 // 
 // 3. Aspas simples = Não expande e não splita
 // 4. Aspas duplas = Expande e não splita
@@ -36,45 +85,52 @@ int expand()
 // t_word *: [{ value: 'echo', quote_state: NONE }]}
 // t_word *: [{ value: '1', quote_state: NONE }]}
 // t_word *: [{ value: '$VAR $USER', quote_state: DOUBLE }
-            // { value: '$VAR', quote_state: SINGLE }
-            // { value: '$VAR', quote_state: NONE }
-            // { value: '$VAR', quote_state: DOUBLE }
-            // { value: '$USER', quote_state: SINGLE }
-            // { value: '$VAR', quote_state: NONE }
+			// { value: '$VAR', quote_state: SINGLE }
+			// { value: '$VAR', quote_state: NONE }
+			// { value: '$VAR', quote_state: DOUBLE }
+			// { value: '$USER', quote_state: SINGLE }
+			// { value: '$VAR', quote_state: NONE }
 // t_word *: [{ value: 'oi', quote_state: NONE }]}
 // Após expansão do ARGV:
 // t_word *: [{ value: 'echo', quote_state: NONE }]}
 // t_word *: [{ value: '1', quote_state: NONE }]}
 // t_word *: [{ { value: 'Hello World', quote_state: NONE }]} <= SPLITA AQUI
-            // value: 'Hello World bmoreira', quote_state: DOUBLE }, 
-            // { value: '$VAR', quote_state: SINGLE }, 
-            // { value: 'Hello World', quote_state: NONE }]} <= SPLITA AQUI
-            // { value: 'Hello World', quote_state: DOUBLE }
-            // { value: 'Hello World', quote_state: NONE }]} <= SPLITA AQUI
-            // { value: '$USER', quote_state: SINGLE }
-            // { value: 'Hello World', quote_state: NONE } <= SPLITA AQUI
-            // { value: 'Hello World Hello Hello', quote_state: NONE } <= SPLITA AQUI
-            // { value: '$USER', quote_state: SINGLE }
-            // { value: 'Hello', quote_state: NONE }]} <= SPLITA AQUI
+			// value: 'Hello World bmoreira', quote_state: DOUBLE }, 
+			// { value: '$VAR', quote_state: SINGLE }, 
+			// { value: 'Hello World', quote_state: NONE }]} <= SPLITA AQUI
+			// { value: 'Hello World', quote_state: DOUBLE }
+			// { value: 'Hello World', quote_state: NONE }]} <= SPLITA AQUI
+			// { value: '$USER', quote_state: SINGLE }
+			// { value: 'Hello World', quote_state: NONE } <= SPLITA AQUI
+			// { value: 'Hello World Hello Hello', quote_state: NONE } <= SPLITA AQUI
+			// { value: '$USER', quote_state: SINGLE }
+			// { value: 'Hello', quote_state: NONE }]} <= SPLITA AQUI
 
 
-            // t_word *: [{ { value: 'Hello, WorldHello World bmoreira$VARHello, WorldHello WorldHello, World$USERHello, WorldHello, World, Hello, Hello$USERHello
+			// t_word *: [{ { value: 'Hello, WorldHello World bmoreira$VARHello, WorldHello WorldHello, World$USERHello, WorldHello, World, Hello, Hello$USERHello
 // while (words)
-    // if (quote_state == NONE)
-        // i+= count_words(value);
-    // word = word->next;
+	// if (quote_state != NONE && !has_content)
+	//  i++;
+	//  has_content = true;
+	// else => quote_state == NONE
+	// if (has_content)
+	//  i+ = (count_words(value) - 1)
+	//  has_content = false;
+	// else
+	//  i+ = count_words(value)
+	// word = word->next;
 // char **argv;
 // malloc = (i+1) + 1
 // while (words)
-    // if (quote_state != NONE)
-        // argv[i] = join(argv[i], value);
-    // if (quote_state == NONE)
-        // splitado = split(value)
-        // argv[i] = join(argv[i], splitado[0]);
-        // while (splitado)
-            // i++;
-            // argv[i] = splitado[i]
-    // 
+	// if (quote_state != NONE)
+		// argv[i] = join(argv[i], value);
+	// if (quote_state == NONE)
+		// splitado = split(value)
+		// argv[i] = join(argv[i], splitado[0]);
+		// while (splitado)
+			// i++;
+			// argv[i] = splitado[i]
+	// 
 // t_word *: [{ value: 'oi', quote_state: NONE }]}
 
 // export VAR="Hello World" && echo 1 "$VAR $USER"'$VAR'$VAR"$VAR"'$USER'$VAR oi
