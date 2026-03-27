e9ff3dffb2d7d6ca6e181d06650c499d8161d378
bmoreira <bmoreira@student.42sp.org.br>
2026-03-27 20:28:26 -0300

    fix: norminette in headers

--- include/heredoc.h
+++ include/heredoc.h
@@ -6,7 +6,7 @@
 /*   By: bmoreira <bmoreira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/07 00:44:52 by bmoreira          #+#    #+#             */
-/*   Updated: 2026/03/27 20:18:57 by bmoreira         ###   ########.fr       */
+/*   Updated: 2026/03/27 20:27:52 by bmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@
 # include "lexer.h"
 # include "parser.h"
 
-int     collect_heredocs(t_ast *tokens);
-int     redirect_heredoc(char **target);
+int		collect_heredocs(t_ast *tokens);
+int		redirect_heredoc(char **target);
 char	**split_content_heredoc(t_word *words);
 
 #endif
