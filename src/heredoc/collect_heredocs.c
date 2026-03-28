4cea50a2bf91b2b84c298ed7f6fce64e1fc00f85
Bruno Costa Borges <bcosta-b@c1r5p3.42sp.org.br>
2026-03-27 23:01:15 -0300

    fix: add delimiter multi word support

--- src/heredoc/collect_heredocs.c
+++ src/heredoc/collect_heredocs.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   collect_heredocs.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bmoreira <bmoreira@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bcosta-b <bcosta-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 21:16:37 by bcosta-b          #+#    #+#             */
-/*   Updated: 2026/03/27 16:56:10 by bmoreira         ###   ########.fr       */
+/*   Updated: 2026/03/27 22:59:26 by bcosta-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 int				collect_heredocs(t_ast *ast);
 static t_word	*collect_input(t_word *delimiter);
+static t_word	*build_delimiter(t_word *words);
 
 int	collect_heredocs(t_ast *ast)
 {
@@ -36,14 +37,34 @@ int	collect_heredocs(t_ast *ast)
 			return (EXIT_FAILURE);
 		redir = (t_redir *) redirs->content;
 		if (ft_strcmp(redir->type, "<<") == 0)
-			redir->target = collect_input(redir->target);
+			redir->target = collect_input(build_delimiter(redir->target));
 		redirs = redirs->next;
 	}
 	return (EXIT_SUCCESS);
 }
 
-static t_word	*collect_input(t_word *delimiter)
+static t_word	*build_delimiter(t_word *words)
 {
+	t_word			*word;
+	char			*string;
+	t_quote_state	quote_state;
+
+	word = words;
+	string = ft_strdup("");
+	quote_state = QUOTE_NONE;
+	while (word)
+	{
+		string = ft_strjoin(string, word->link.content);
+		if (word->quote_state == QUOTE_SINGLE
+			|| word->quote_state == QUOTE_DOUBLE)
+			quote_state = QUOTE_DOUBLE;
+		word = (t_word *) word->link.next;
+	}
+	return (create_word(string, ft_strlen(string), quote_state));
+}
+
+static t_word	*collect_input(t_word *delimiter)
+{	
 	t_word	*words;
 	char	*line;
 
